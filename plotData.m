66bcc958d9047afcc2f8eb09b840d6328b113dcb
Muhammad Faizan <61932757+faizan1234567@users.noreply.github.com>
2020-11-05 10:28:16 +0500

    Add files via upload
    Logistic Regression, a powerful classification algorithm for classifying different classes. In this file cost function, gradient computation and optimization algorithm (gradient descent ) have been solved in Matlab 2019b.

--- plotData.m
+++ plotData.m
@@ -1,28 +1,31 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-figure; % open a new figure window
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
-plot(x,y,'rx','MarkerSize',10);
-xlabel('population size in 10000s');
-ylabel('profit in $1000 ');
+pos = find(y==1) ; neg = find(y==0);
+plot(X(pos,1),X(pos,2),'LineWidth',2,'k+','MarkerSize',7);
+plot(X(neg,1),X(neg,2),'ko','MarkerFaceColor','y',...
+    'MarkerSize',7);
 
 
 
 
-% ============================================================
+
+
+% =========================================================================
+
+
+
+hold off;
 
 end
